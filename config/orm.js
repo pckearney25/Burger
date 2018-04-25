73dfed702438a1238b64af07ce7f42a21a7702aa
Patrick Kearney <pckearney@yahoo.com>
2018-04-25 15:31:48 -0500

    initial connections between files

---
+++ config/orm.js
@@ -0,0 +1,10 @@
+var connection = require("./connection.js");
+
+module.exports = function(connection) {
+  //create the methods that will execute the necessary MySQL commands in
+  //the controllers. These are the methods you will need to use in order to retrieve and store data
+  //in your database.
+  //* `selectAll()` * `insertOne()` * `updateOne()`
+};
+
+//not sure if this is correct. May need to export each individ? I SUSPECT SO.
